261237226ad219493feafc25ab3ea7936c66e821
KingZuko <rs6121@nyu.edu>
2024-09-05 00:20:57 -0700

    vim file updated fror practice

---
+++ vim.py
@@ -0,0 +1,44 @@
+"""
+Welcome to Vim Practice!
+========================
+
+1. Basic Movement
+   - Use h, j, k, l to move the cursor
+   - Practice moving between words with w, b, e
+
+2. Editing Text
+   - Insert mode: i, a, o, O
+   - Delete: x, dd, dw
+   - Change: c, cc, cw
+   - Yank (copy): y, yy, yw
+   - Paste: p, P
+
+3. Search and Replace
+   - Search forward: /pattern
+   - Search backward: ?pattern
+   - Replace: :s/old/new/g
+
+4. Working with Multiple Files
+   - Open new file: :e filename
+   - Split window: :sp, :vsp
+   - Switch between windows: Ctrl-w + h, j, k, l
+
+5. Advanced Commands
+   - Macros: q{register}, @{register}
+   - Marks: m{a-z}, '{a-z}
+   - Folding: zf, zo, zc
+
+Practice Area:
+--------------
+The quick brown fox jumps over the lazy dog.
+She sells seashells by the seashore.
+How much wood would a woodchuck chuck if a woodchuck could chuck wood?
+
+TODO:
+[ ] Delete this line
+[ ] Change "fox" to "cat" in the first sentence
+[ ] Copy the second sentence and paste it below
+[ ] Replace all instances of "wood" with "stone"
+
+Happy Vim-ing!
+"""
