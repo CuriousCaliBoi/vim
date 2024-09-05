5778e355781cf7ac42bfe903892bc79eb10a81f8
KingZuko <rs6121@nyu.edu>
2024-09-05 00:31:38 -0700

    Add Vim practice guide with basic commands and concepts
    This commit introduces a comprehensive Vim practice guide in vim.py. The guide covers:
    - Basic movement commands
    - Text editing operations
    - Search and replace functionality
    - Working with multiple files
    - Advanced commands like macros and marks
    - Text objects and visual mode
    - Command-line mode operations

--- vim.py
+++ vim.py
@@ -28,6 +28,44 @@ Welcome to Vim Practice!
    - Marks: m{a-z}, '{a-z}
    - Folding: zf, zo, zc
 
+6. Text Objects
+   - Inner word: iw
+   - Around word: aw
+   - Inner sentence: is
+   - Around sentence: as
+   - Inner paragraph: ip
+   - Around paragraph: ap
+
+7. Visual Mode
+   - Character-wise visual mode: v
+   - Line-wise visual mode: V
+   - Block-wise visual mode: Ctrl-v
+
+8. Command-line Mode
+   - Enter command-line mode: :
+   - Save file: :w
+   - Quit Vim: :q
+   - Save and quit: :wq or :x
+
+9. Registers
+   - Copy to register: "{register}y
+   - Paste from register: "{register}p
+   - View registers: :reg
+
+10. Undo and Redo
+    - Undo: u
+    - Redo: Ctrl-r
+
+11. Plugins and Customization
+    - Installing plugins: Mention popular plugin managers (e.g., Vim-Plug, Vundle)
+    - Customizing .vimrc: Brief explanation of how to add personal configurations
+
+12. Vim Modes
+    - Normal mode: Default mode for navigation and commands
+    - Insert mode: For typing and editing text
+    - Visual mode: For selecting text
+    - Command-line mode: For entering Ex commands
+
 Practice Area:
 --------------
 The quick brown fox jumps over the lazy dog.
@@ -39,6 +77,25 @@ TODO:
 [ ] Change "fox" to "cat" in the first sentence
 [ ] Copy the second sentence and paste it below
 [ ] Replace all instances of "wood" with "stone"
+
+Additional Practice Problems:
+1. [ ] Insert a new line at the beginning of the Practice Area
+2. [ ] Capitalize all words in the second sentence
+3. [ ] Delete the third sentence
+4. [ ] Indent all lines in the Practice Area by 4 spaces
+5. [ ] Join the first two lines into one sentence
+6. [ ] Swap the positions of "quick" and "brown" in the first sentence
+7. [ ] Convert all lowercase 'a' to uppercase 'A' in the Practice Area
+8. [ ] Add line numbers to each line in the Practice Area
+9. [ ] Reverse the order of words in the second sentence
+10. [ ] Duplicate the entire Practice Area and append it to the end
+11. [ ] Use a macro to add a prefix to each line in the Practice Area
+12. [ ] Create a vertical split and move half of the Practice Area to the new window
+13. [ ] Use text objects to quickly modify content within quotes or parentheses
+14. [ ] Practice using marks to quickly jump between different parts of the file
+15. [ ] Experiment with different types of registers for copying and pasting
+
+Remember to use Vim commands to practice these tasks!
 
 Happy Vim-ing!
 """
